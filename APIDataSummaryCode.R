2302878c7262de56364fda6e79648aa9324a69e7
aliczner <aliczner@gmail.com>
2026-06-05 10:40:39 -0400

    added in meta data for species and receivers

--- APIDataSummaryCode.R
+++ APIDataSummaryCode.R
@@ -23,11 +23,16 @@ sql_motus <- tagme(projRecv = proj.num,
                    skipNodes = TRUE, 
                    skipDeprecated = TRUE)
 
+sql_motus <- tagme(proj.num, 
+                   skipNodes = TRUE, 
+                   skipDeprecated = TRUE)
+metadata(sql_motus) #needed to get the metadata
+
 ## testing Sauble Beach Tower
 
 sql_motus.sauble <- tagme(projRecv = "CTT-V3023D10185C",
-                          new = TRUE) #started at 3:22 pm
-
+                          new = TRUE) 
+metadata(sql_motus.sauble)
 
 # ================================================
 # getting detection data
@@ -38,19 +43,58 @@ library(RSQLite)
 
 dbListTables(sql_motus) #to view tables
 dbListFields(sql_motus, "alltags") #to view fields of tables
-dbListFields(sql_motus, "species")
 
 alltags.tbl <- tbl(sql_motus, "alltags") #convert to table
-species.table <- tbl(sql_motus, "species")
+deps.tbl <- tbl(sql_motus, "tagDeps")
+sp.tbl <- tbl(sql_motus, "species")
+tagProp.tbl <- tbl(sql_motus, "tagProps")
+recvDep.tbl <- tbl(sql_motus, "recvDeps")
 
 alltags.df <- alltags.tbl %>% 
   collect() %>% 
   as.data.frame
 
-species.df <- species.table %>% 
+write.csv (alltags.df, "UWO-SSCAllTags.csv")
+
+deps.df <- deps.tbl %>% 
   collect() %>% 
   as.data.frame
 
+write.csv (deps.df, "UWO-SSCDeps.csv")
 
+recvDeps.df <- recvDep.tbl %>% 
+  collect() %>% 
+  as.data.frame
 
-write.csv(alltags.df, "alltagstest.csv")
+#====================================================
+# adding additional bird metadata
+#====================================================
+
+#adding sex and age where possible
+alltags.deps <- alltags.df %>%
+  left_join(
+    deps.df %>% 
+      select(tagID, speciesID, markerNumber, sex, age),
+    by = c(
+      "motusTagID" = "tagID",        
+      "speciesID" = "speciesID",    
+      "markerNumber" = "markerNumber"  
+    )
+  )
+
+##adding in receiver data
+alltags.recvs <- alltags.deps %>% 
+#Drop the empty columns from the main data so there's no naming conflict
+  select(-recvSiteName, -recvDeployLat, -recvDeployLon) %>% 
+  #now join
+  left_join(
+    recvDeps.df %>% 
+      select(serno, name, stationLat, stationLon) %>% 
+      rename(
+        recvSiteName  = name,
+        recvDeployLat = stationLat,
+        recvDeployLon = stationLon
+      ), 
+    by = c("recv" = "serno")
+  )
+  
