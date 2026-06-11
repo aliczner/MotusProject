caec82b777c14aaa3ae6fb4caa910fcd53727b37
aliczner <aliczner@gmail.com>
2026-06-11 17:34:54 +0000

    edited to be stationID not stationName

--- stationSelection.R
+++ stationSelection.R
@@ -21,7 +21,7 @@ filtered_pairs <- station_pairs %>%
     previousStationName != "Unknown",
     previousStationName != "Unknown station"
   )
-#21940 obs
+#21942 obs
 
 #loading great lakes watershed polygon, contains subbasins for each lake
 GLWatershed <- st_read("./greatlakes_subbasins/greatlakes_subbasins.shp")
@@ -77,7 +77,7 @@ filtered_df <- filtered_pairs %>%
 
 str(filtered_df) #9214 obs
 
-write.csv(filtered_df, "StationPairsFilterd.csv")
+write.csv(filtered_df, "StationPairsFiltered.csv")
 
 ### creating a map of the points
 
@@ -106,3 +106,107 @@ mapview(GLWS_proj,
   mapview(prev_map_GLWS, 
           col.regions = "orange",
           layer.name = "Previous Station")
+
+#================================================================
+# seeing the number of unique station pairs and unique stations
+#================================================================
+
+pair_counts_by_season <- filtered_df %>%
+  group_by(season) %>%
+  summarise(
+    unique_pairs_count = n_distinct(previousStationID, stationID)
+  )
+
+(pair_counts_by_season)
+#season           unique_pairs_count
+#<chr>                         <int>
+#  1 Fall Migration               3064
+#2 Spring Migration               2313
+#3 Summer                         1387
+#4 Winter                          719
+
+unique_stations_by_season <- filtered_df %>%
+  select(season, previousStationID, stationID) %>%
+  pivot_longer(
+    cols = c(previousStationID, stationID), 
+    values_to = "all_stations"
+  ) %>%
+  group_by(season) %>%
+  summarise(
+    unique_stations_count = n_distinct(all_stations)
+  )
+unique_stations_by_season
+#season           unique_stations_count
+#<chr>                            <int>
+#  1 Fall Migration                   637
+#2 Spring Migration                   543
+#3 Summer                             378
+#4 Winter                             300
+
+#=================================================================
+# getting less refined data
+#================================================================
+
+station_pairs_collapsed <- filtered_df %>%
+#Group by the columns that define the paired stations
+  group_by(
+    previousStationID, 
+    stationID, 
+    season,
+    lat, 
+    lon, 
+    lat_previous, 
+    lon_previous,
+    distance_km, 
+  ) %>%
+  
+# Collapse the rows and count the number of unique species for each pair
+  summarise(
+    stationName = first(stationName),
+    previousStationName = first(previousStationName),
+    number_of_species = n_distinct(species),
+    
+    #Sum the total movements across all species for this pair of stations
+    total_movements   = sum(number_of_movements, na.rm = TRUE),
+    .groups = "drop"
+  )
+station_pairs_collapsed
+
+write.csv(station_pairs_collapsed, "StationPairsCollapsed.csv")
+
+# counting movements for individual stations
+
+# Isolate the previous station data
+prev_station <- filtered_df %>%
+  select(
+    station_ID = previousStationID, 
+    stationName = previousStationName, 
+    season, 
+    species,
+    number_of_movements
+  )
+
+# Isolate the current tower data
+current_station <- filtered_df %>%
+  select(
+    station_ID = stationID, 
+    stationName = stationName,
+    season, 
+    species, 
+    number_of_movements
+  )
+
+# Combine them, group by station/season, and calculate summaries
+individual_station_summary <- bind_rows(prev_station, 
+                                       current_station) %>%
+  group_by(station_ID, 
+           season) %>%
+  summarise(
+    # Count how many unique species visited this specific station
+    stationName = first(stationName),
+    number_of_species = n_distinct(species),
+    
+    # Sum up all movements passing through this specific station
+    total_movements   = sum(number_of_movements)
+    )
+write.csv(individual_station_summary, "StationIndividualSummary.csv")
