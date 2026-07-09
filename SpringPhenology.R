bc5234e68aaec5aac69d23ee0041906dfb682626
aliczner <aliczner@gmail.com>
2026-07-09 17:14:56 -0400

    added in map, table of species number per subbasin and phenology plot for species in all 5 basins

--- SpringPhenology.R
+++ SpringPhenology.R
@@ -183,4 +183,99 @@ for (target_subbasin in subbasin_list) {
 
 }
 
+#================================================================
+# map of spring first arrival points
+#=================================================================
 
+library(sf)
+library(sfheaders)
+library(mapview)
+
+# springTable doesn't have coords so need to add that in 
+first_arrival_paths <- springData %>%
+  mutate(
+    tsStart_dt = as_datetime(tsStart_dt, 
+                             tz = "GMT"),
+    subbasin = if_else(subbasin == "geo_bay", 
+                       "lk_huron", subbasin)
+  ) %>%
+  semi_join(springTable, 
+            by = c("species", 
+                   "subbasin", 
+                   "year_start", 
+                   "tsStart_dt" = "first_arrival")) 
+
+# separate current and previous station coords
+pts_previous <- st_as_sf(first_arrival_paths, 
+                         coords = c("lon_previous", 
+                                    "lat_previous"), 
+                         crs = 4326)
+pts_current  <- st_as_sf(first_arrival_paths, 
+                         coords = c("lon", 
+                                    "lat"), 
+                         crs = 4326)
+
+# make the paths, setting to TRUE goes by row not distance
+paths_first_arrival.sf <- pts_previous %>% 
+  mutate(geometry = st_nearest_points(st_geometry(pts_previous), 
+                                      st_geometry(pts_current), 
+                                      pairwise = TRUE))
+#add label for mapping
+pts_prev_labeled <- pts_previous %>% mutate(location_type = "Previous")
+pts_curr_labeled <- pts_current  %>% mutate(location_type = "Current")
+
+combined_stations.sf <- bind_rows(pts_prev_labeled, 
+                                  pts_curr_labeled) %>% 
+  mutate(location_type = factor(location_type, 
+                                levels = c("Previous", 
+                                           "Current"))) %>% 
+  st_as_sf()
+
+subbasin_list <- c("lk_erie", 
+                   "lk_ont", 
+                   "lk_mich", "
+                   lk_huron", 
+                   "lk_sup")
+
+final_map <- mapview()
+
+# need to loop through the subbasins so they can be toggled in map
+for (target_subbasin in subbasin_list) {
+  
+  # filter lines and stations
+  subbasin_lines <- paths_first_arrival.sf %>% 
+    filter(subbasin == target_subbasin)
+  subbasin_dots  <- combined_stations.sf %>%
+    filter(subbasin == target_subbasin)
+  
+  if (nrow(subbasin_dots) == 0) next
+  
+  # make unique names so they don't overwrite each other
+  lines_name <- paste0(target_subbasin, 
+                       " (Lines)")
+  dots_name  <- paste0(target_subbasin,
+                       " (Stations)")
+  
+  # Add lines and dots to the final map layout (hidden by default)
+  final_map <- final_map + 
+    mapview(
+      subbasin_lines, 
+      color = "black", 
+      lwd = 1.5, 
+      layer.name = lines_name,
+      hide = TRUE
+    ) + 
+    mapview(
+      subbasin_dots,
+      zcol = "location_type", 
+      #brown for previous, teal for current
+      col.regions = c("Start" = "#846C5B", "End" = "#6DD3CE"), 
+      size = 4,
+      legend = FALSE,         
+      layer.name = dots_name,
+      hide = TRUE
+    )
+}
+
+# Render the interactive map
+final_map
