f9fca9be1b508e0381c3d67e9dbbfde6cabbafcf
aliczner <aliczner@gmail.com>
2026-07-28 17:19:07 -0400

    tried making line KDE for flights where the tagging site was outside of the GLWS

--- SpringFlights.R
+++ SpringFlights.R
@@ -5,6 +5,11 @@ library(lubridate)
 library(ggplot2)
 library(ggridges)
 library(sf)
+library(sfheaders)
+library(spatstat.geom)
+library(spatstat.explore)
+library(terra)
+
 
 fullData <- read.csv ("StationPairsFiltered.csv")
 
@@ -44,7 +49,7 @@ flight_lines <- st_sf(flight_steps,
                                         crs = 4326))
 
 #======================================================
-# line kernel density
+# line kernel density all flights
 # ====================================================
 
 library(spatstat.geom)
@@ -152,7 +157,108 @@ mapview(
     na.color = "transparent"
   )
 
-##sigma 10 km is the best output
+##sigma 10 km is the best output, use that going forward
 
-# Save it
-terra::writeRaster(rast_10km, "flights_all_10km.tif")
+# Save it but what I made was for plotting
+writeRaster(rast_10km, "Plotting_flights_all_10km.tif")
+
+#need to save the raw data 
+
+density_data <- density.psp(
+  flights_psp, 
+  weights = marks(flights_psp), #this keeps the weights from above
+  sigma = 10000, 
+  eps = 1000
+)
+
+density_data.rast <- rast(density_data)
+crs(density_data.rast) <- "EPSG:3978"
+terra::writeRaster(density_data.rast, "Raw_flights_all.tif", 
+                   overwrite=T)
+
+#=============================================
+# line kernel density - tagging outside GLWS
+#============================================
+flight_steps.sf <- flight_steps %>%
+  filter(!is.na(lon_tagSite) & !is.na(lat_tagSite)) %>%
+  st_as_sf(
+    coords = c("lon_tagSite", "lat_tagSite"),
+    crs = 4326,
+    remove = FALSE
+  )
+
+GLWatershed_matched <- st_transform(GLWatershed, 
+                                    crs = 4326)
+
+tagged_outside <- st_filter(
+  flight_steps.sf, 
+  GLWatershed_matched, 
+  .predicate = st_disjoint)
+
+
+geoms <- lapply(seq_len(nrow(tagged_outside)), function(i) {
+  st_linestring(rbind(
+    c(tagged_outside$lon_previous[i], tagged_outside$lat_previous[i]),
+    c(tagged_outside$lon[i],          tagged_outside$lat[i])
+  ))
+})
+
+
+tagout_flight_lines <- st_sf(
+  st_drop_geometry(tagged_outside),
+  geometry = st_sfc(geoms, crs = 4326))
+
+#creating flight lines density
+tagout_flight.pj <- st_transform(tagout_flight_lines, crs = 3978)
+
+tagout_flight.pj <- tagout_flight.pj %>%
+  group_by(previousStationName, stationName) %>%
+  mutate(
+    path_freq = n(),
+    # Square Root Distance Weighting
+    weight_sqrt_dist = path_freq / (distance_km^0.5)
+  ) %>% 
+  ungroup()
+
+tagout_psp <- as.psp(st_geometry(tagout_flight.pj))
+marks(tagout_psp) <- tagout_flight.pj$weight_sqrt_dist
+
+#line kernel density 
+
+tagout.kde <- density.psp(
+  tagout_psp, 
+  weights = marks(tagout_psp), #this keeps the weights from above
+  sigma = 10000, 
+  eps = 1000
+)
+tagout.rast<- terra::rast(tagout.kde)
+crs(tagout.rast) <- "EPSG:3978"
+
+writeRaster(tagout.rast, "LineKDE_TaggedOut.tif")
+
+#longpoint is still too hot to see anything else
+
+# cutoff to avoid log(0) -> -Inf
+tagout.rast[tagout.rast < 1e-10] <- NA
+
+# Log transform the density values
+tagout.rast_log <- log(tagout.rast)
+
+# Set the lower bound anchor (log of 1e-7)
+fixed_min <- log(1e-7)
+
+# Get the maximum value directly from the log-transformed raster
+l_max <- max(terra::values(tagout.rast_log), na.rm = TRUE)
+
+# Rescale values onto the 0-100 index
+r_index <- ((tagout.rast_log - fixed_min) / (l_max - fixed_min)) * 100
+
+# Clamp baseline noise to 0
+r_index[r_index <= 0] <- NA
+
+
+# Render map
+mapview(
+  r_index,
+  col.regions = viridis::inferno(100)
+)
